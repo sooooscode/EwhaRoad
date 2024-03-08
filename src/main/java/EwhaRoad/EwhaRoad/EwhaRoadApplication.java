af83a7149b68dc1f2710e7d2544da912fb4c5d50
soo <sooooscode@gmail.com>
2024-03-08 21:48:49 +0900

    first commit

---
+++ src/main/java/EwhaRoad/EwhaRoad/EwhaRoadApplication.java
@@ -0,0 +1,13 @@
+package EwhaRoad.EwhaRoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwhaRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwhaRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EwhaRoad/EwhaRoad/EwhaRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package EwhaRoad.EwhaRoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhaRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
