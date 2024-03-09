ac812c9dd202bd978a4568eae5760f8f683a4dbf
devhaeun <lala819010@gmail.com>
2024-03-09 22:35:16 +0900

    24-03-09 SetDestinations html 작업 중

--- src/main/frontEnd/SetDestinations/SetDestinations.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.swiper', {
-    speed: 500,
-    spaceBetween: 10,
-  });
-
-const $swiper = document.querySelector('.swiper').swiper;
-
-// Now you can use all slider methods like
-$swiper.slideNext();
