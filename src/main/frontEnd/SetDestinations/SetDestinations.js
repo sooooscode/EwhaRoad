8a03f4af69136fdb5f6b51d73c81d82509338456
devhaeun <lala819010@gmail.com>
2024-03-09 18:16:04 +0900

    시작 페이지 완성, 버스 정보 페이지 구현 중, SetDestinations 페이지 구현 중

---
+++ src/main/frontEnd/SetDestinations/SetDestinations.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    spaceBetween: 10,
+  });
+
+const $swiper = document.querySelector('.swiper').swiper;
+
+// Now you can use all slider methods like
+$swiper.slideNext();
